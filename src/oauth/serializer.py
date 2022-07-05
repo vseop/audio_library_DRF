fe3799064606ccbf1ac90ad7b1ecd1ff030f74ba
vseop <ivpython@mail.ru>
2022-07-05 20:17:27 +0300

    views: author, link serializers: author, link

--- src/oauth/serializer.py
+++ src/oauth/serializer.py
@@ -9,8 +9,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('avatar', 'country', 'city', 'bio', 'display_name')
 
 
-class GoogleAuth(serializers.Serializer):
-    """Сериализация данных от Google"""
+class SocialLinkSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
 
+    class Meta:
+        model = models.SocialLink
+        fields = ('id', 'link')
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    social_links = SocialLinkSerializer(many=True)
+
+    class Meta:
+        model = models.AuthUser
+        fields = ('id', 'avatar', 'country', 'city', 'bio', 'display_name', 'social_links')
+
+
+class GoogleAuth(serializers.Serializer):
+    """ Сериализация данных от Google
+    """
     email = serializers.EmailField()
     token = serializers.CharField()
--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -3,6 +3,13 @@ from .endpoint import views, auth_views
 
 urlpatterns = [
     path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
+
+    path('author/', views.AuthorView.as_view({'get': 'list'})),
+    path('author/<int:pk>/', views.AuthorView.as_view({'get': 'retrieve'})),
+
+    path('social/', views.SocialLinkView.as_view({'get': 'list', 'post': 'create'})),
+    path('social/<int:pk>/', views.SocialLinkView.as_view({'put': 'update', 'delete': 'destroy'})),
+
     path('google/', auth_views.google_auth),
     path('', auth_views.google_login)
 ]
