6c47f4a9d29a7c51728e1e325b72e50787f31a83
vseop <ivpython@mail.ru>
2022-07-06 10:56:06 +0300

    Revert "CRUD album"
    This reverts commit f6425235afd337c36c2a9d973c47766eee54ffed.

--- src/audio_library/urls.py
+++ src/audio_library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
     path('genre/', views.GenreView.as_view()),
@@ -7,9 +7,5 @@ urlpatterns = [
     path('license/', views.LicenseView.as_view({'get': 'list', 'post': 'create'})),
     path('license/<int:pk>/', views.LicenseView.as_view({'put': 'update', 'delete': 'destroy'})),
 
-    path('album/', views.AlbumView.as_view({'get': 'list', 'post': 'create'})),
-    path('album/<int:pk>/', views.AlbumView.as_view({'put': 'update', 'delete': 'destroy'})),
-
-    path('author-album/<int:pk>/', views.PublicAlbumView.as_view()),
 
 ]
