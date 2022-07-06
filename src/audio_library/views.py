f6425235afd337c36c2a9d973c47766eee54ffed
vseop <ivpython@mail.ru>
2022-07-06 10:54:57 +0300

    CRUD album

--- src/audio_library/views.py
+++ src/audio_library/views.py
@@ -1,12 +1,14 @@
 import os
 
-
+from django.http import FileResponse, Http404, HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets, parsers, views
 
 from . import models, serializer
-
+from ..base.classes import MixedSerializer, Pagination
 from ..base.permissions import IsAuthor
-
+from ..base.services import delete_old_file
 
 
 class GenreView(generics.ListAPIView):
@@ -29,3 +31,28 @@ class LicenseView(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
+class AlbumView(viewsets.ModelViewSet):
+    """ CRUD альбомов автора
+    """
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class = serializer.AlbumSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        delete_old_file(instance.cover.path)
+        instance.delete()
+
+
+class PublicAlbumView(generics.ListAPIView):
+    """ Список публичных альбомов автора
+    """
+    serializer_class = serializer.AlbumSerializer
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user__id=self.kwargs.get('pk'), private=False)
