3ca1c40d4fbfce130253fe11e2d0f1b8c05b67e0
vseop <ivpython@mail.ru>
2022-07-06 10:59:18 +0300

    CRUD album end

--- src/audio_library/serializer.py
+++ src/audio_library/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from . import models
-
+from ..base.services import delete_old_file
 
 
 class BaseSerializer(serializers.ModelSerializer):
@@ -19,3 +19,11 @@ class LicenseSerializer(BaseSerializer):
         fields = ('id', 'text')
 
 
+class AlbumSerializer(BaseSerializer):
+    class Meta:
+        model = models.Album
+        fields = ('id', 'name', 'description', 'cover', 'private')
+
+    def update(self, instance, validated_data):
+        delete_old_file(instance.cover.path)
+        return super().update(instance, validated_data)
--- src/audio_library/views.py
+++ src/audio_library/views.py
@@ -6,7 +6,7 @@ from rest_framework import generics, viewsets, parsers, views
 from . import models, serializer
 
 from ..base.permissions import IsAuthor
-
+from ..base.services import delete_old_file
 
 
 class GenreView(generics.ListAPIView):
@@ -27,5 +27,31 @@ class LicenseView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class AlbumView(viewsets.ModelViewSet):
+    """ CRUD альбомов автора
+    """
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class = serializer.AlbumSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        delete_old_file(instance.cover.path)
+        instance.delete()
+
+
+class PublicAlbumView(generics.ListAPIView):
+    """ Список публичных альбомов автора
+    """
+    serializer_class = serializer.AlbumSerializer
+
+    def get_queryset(self):
+        return models.Album.objects.filter(user__id=self.kwargs.get('pk'), private=False)
 
 
